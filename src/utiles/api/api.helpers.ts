0b5de4a7bfec39495302dc151d29f008c0dfb7ce
NickZavadski <nick.mr.volk@gmail.com>
2024-02-27 00:47:14 +0300

    fix: not work api with new date

--- src/utiles/api/api.helpers.ts
+++ src/utiles/api/api.helpers.ts
@@ -3,9 +3,11 @@ import moment from 'moment'
 
 import { API_KEY } from '../constants/api'
 
-
 export const getContentType = () => ({
     'Content-Type': 'application/json',
 })
 
-export const getAuthHash = () => md5(`${API_KEY}_${moment().format('YYYYMMDD')}`)
+// Todo: временно
+export const getAuthHash = () => md5(`${API_KEY}_${moment().format('YYYYMMDD').replace('7', '6')}`)
+
+// export const getAuthHash = () => md5(`${API_KEY}_${moment().format('YYYYMMDD')}`)
